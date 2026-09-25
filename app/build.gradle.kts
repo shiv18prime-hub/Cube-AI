947397764fd4489ead8423258b26e7e231c80efc
shiv18prime-hub <shiv18prime@gmail.com>
2026-09-25 18:23:42 +0530

    Fix JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,4 +12,11 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
